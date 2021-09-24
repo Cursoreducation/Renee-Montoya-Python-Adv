206a018783dd8bacbc61c5cc4a149b5582da8616
Kitty2221 <katiagolub56@gmail.com>
2021-09-24 19:09:12 +0300

    Revert "move save,generate_dict to abstract midel class"
    This reverts commit d12044e90c16af1846bde8e55dbe26731cb3df9a.

--- lectures/oop/ReneeMontoyaApp/framework/models.py
+++ lectures/oop/ReneeMontoyaApp/framework/models.py
@@ -7,15 +7,11 @@ class Model(ABC):
 
     @abstractmethod
     def save(self):
-        obj_in_dict_format = self._generate_dict()
-        objs = self.get_file_data(self.file)
-        objs.append(obj_in_dict_format)
-        self.save_to_file(objs)
+        pass
 
     @abstractmethod
     def _generate_dict(self):
         pass
-        return {field_name: getattr(self, field_name) for field_name in self.__dict__}
 
     @classmethod
     def get_by_id(cls, id):
