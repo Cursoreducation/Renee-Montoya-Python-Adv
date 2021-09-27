f7e9c8919b3ddc8caf0e260b4ffaedbf23a97107
Vitalii <vitalikkalyta96@gmail.com>
2021-09-28 00:03:43 +0300

    Completed HW_4(updated).

--- homework/tests_practice/testing/test_employee.py
+++ homework/tests_practice/testing/test_employee.py
@@ -1,12 +1,12 @@
-import unittest
 from tests.employee import Employee
-from unittest import TestCase
 import pytest
+from unittest import TestCase
+from unittest.mock import patch
 
 
 @pytest.fixture()
 def user():
-     print("Jack")
+    print("Denzel")
 
 
 @pytest.mark.usefixtures("user")
@@ -23,25 +23,59 @@ def test_1(first, last, pay):
 
 class TestEmployee(TestCase):
 
-    @unittest.skip(reason="skip")
-    def test_email_2(self):
-        employee_ = Employee("john", 'recon',50)
-        employee_.email = "jack", "salivan"
-        self.assertIn("jack", (employee_.email()))
+    def setUp(self):
+        self.employee = Employee("Jack", "Sallivan", 200)
 
-CONSTANT = False
+    def test_check_first_name_01(self):
+        self.assertEqual(self.employee.first, "Jack")
 
+    def test_check_first_name_02(self):
+        self.assertNotEqual(self.employee.first, "John")
 
-@pytest.mark.skipif(CONSTANT is False, reason="skip")
-def test_monthly_schedule_3(requests_mock):
-    requests_mock.get(
-        "http://jsonplaceholder.typicode.com/tsodos", text="data", status_code=200
-    )
-    classForTest = Employee("jack", "salivan", 50)
-    assert classForTest.monthly_schedule(2).ok
-    assert classForTest.monthly_schedule(2).status_code == 200
+    def test_check_last_name_03(self):
+        self.assertEqual(self.employee.last, "Sallivan")
+
+    def test_check_last_name_04(self):
+        self.assertNotEqual(self.employee.last, "Sally")
+
+    def test_check_correct_pay_05(self):
+        self.assertEqual(self.employee.pay, 200)
+
+    def test_property_email_06(self):
+        self.assertEqual(self.employee.email, 'Jack.Sallivan@email.com')
+        self.employee.first = "Denzel"
+        self.employee.last = "Rayan"
+        self.assertEqual(self.employee.email, 'Denzel.Rayan@email.com')
+
+    def test_property_fullname_07(self):
+        self.employee.first = "Arnold"
+        self.employee.last = "Schwerzer"
+        self.assertEqual(self.employee.fullname, 'Arnold Schwerzer')
+
+    def test_apply_raise_08(self):
+        self.employee.apply_raise()
+        self.assertNotEqual(self.employee.pay, 209)
+        self.assertEqual(self.employee.pay, 210)
+
+    @patch('requests.get')
+    def test_false_monthly_schedule_09(self, mocker_):
+        class MockerFalse():
+            ok = False
+            text = "Arnold Schwerzer"
+
+        mocker_.return_value = MockerFalse()
+        self.assertEqual(self.employee.monthly_schedule(1), 'Bad Response!')
+
+    @patch('requests.get')
+    def test_monthly_schedule_10(self, mocker):
+        class Mocker():
+            ok = True
+            text = "Arnold Schwerzer"
+
+        mocker.return_value = Mocker()
+        self.assertEqual(self.employee.monthly_schedule(10), "Arnold Schwerzer")
 
 
 if __name__=="__main__":
-    TestEmployee()
     user()
+    TestEmployee()
